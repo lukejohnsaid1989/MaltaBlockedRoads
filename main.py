f4a09544d1cc70379958a0d49d6fc07e38bbf46e
Luke Said <57299495+lukejohnsaid1989@users.noreply.github.com>
2026-03-25 11:09:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -32,10 +32,10 @@ df_filtered = df[df['date'].dt.date.isin(selected_dates)].copy()
 st.subheader(f"Showing {len(df_filtered)} closed roads")
 
 explanation_text = [
-"This is not an official government website. It is a personal project with the intent of visualising closed roads.",
+"This is not an official government website. It is a personal project with the intent of visualising closed roads. ",
 "Road closure information is copied and pasted from here: https://pulizija.gov.mt/en/general-notices/ ",
-"Latitude and Lognitude are exracted from here: https://nominatim.openstreetmap.org/search."
-"There is no official guarantee that the information here is accurate. It is the result of a python automation that processes the copied information.",
+"Latitude and Lognitude are exracted from here: https://nominatim.openstreetmap.org/search. "
+"There is no official guarantee that the information here is accurate. It is the result of a python automation that processes the copied information. ",
 "No AI is used in the processing.", "No data automated scraping is used."
 ]
 
@@ -86,4 +86,4 @@ deck = pdk.Deck(
 )
 
 st.pydeck_chart(deck, use_container_width=True)
-st.write(" ".join(explanation_text))
+st.write("".join(explanation_text))
